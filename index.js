56d5635f00d53b390bfc8f99d1399d85edb750d3
Snel <snelson10@sfccmo.edu>
2018-03-06 15:58:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
  // write your code here
+ function getRandomNumber(min, max) {
+  var number = Math.random() * (max - min) + min;
+  return Math.floor(number);
+}
+ var itemPrice = getRandomNumber(1, 100);
+ var newItem = {[itemName]: itemPrice}
+ cart.push(newItem);
+ console.log(itemName + ' has been added to your cart.')
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0){
+    console.log('Your shopping cart is empty.')
+  } else {
+    cart = cart.map(i => )
+  }
 }
 
 function total() {
